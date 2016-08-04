a983f4482202d5506a87b32551934dd6d4e7dfd5
gaogang <gaogang@asiainfo.com>
2016-08-04 15:27:29 +0800

    添加静态代码测试

---
+++ src/com/test/queue/StaticTest.java
@@ -0,0 +1,14 @@
+package com.test.queue;
+
+public class StaticTest {
+
+	static{
+		System.out.println("静态代码块");
+	}
+	public static void initApp(){
+		System.out.println("进入静态方法！");
+	}
+	public static void main(String[] args) {
+		System.out.println("main");
+	}
+}
