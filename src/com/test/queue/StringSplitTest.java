be24a2f421f9d4f39c3a83488103536936dca760
gaogang <gaogang@asiainfo.com>
2016-08-16 15:39:12 +0800

    数组转成字符串

---
+++ src/com/test/queue/StringSplitTest.java
@@ -0,0 +1,16 @@
+package com.test.queue;
+
+public class StringSplitTest {
+	public static void main(String[] args) {
+		String time = "2016-12-13";
+		
+		String[] s=time.split("-");
+		StringBuffer sb1=new StringBuffer();
+		for(int i=0;i<s.length;i++){
+			sb1.append(s[i]);
+		}
+		String ss=sb1.toString()+"000000";
+		//String a = new String(time.split("-"))+ "000000";
+		System.out.println(ss);
+	}
+}
