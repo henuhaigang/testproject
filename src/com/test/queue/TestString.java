51164ec09f530512e783a0f5ad27ad56c1976901
gaogang <gaogang@asiainfo.com>
2016-08-25 14:17:48 +0800

    添加String和Integer的测试类

---
+++ src/com/test/queue/TestString.java
@@ -0,0 +1,27 @@
+package com.test.queue;
+/**
+ * String,Integer 对象创建测试类
+ * @author scorpion
+ *
+ */
+public class TestString {
+
+	public static void main(String[] args) {
+		String str1 = new String("abc"); 
+		String str2 = new String("abc"); 
+		String str3 = "abc"; 
+		String str4 = "abc"; 
+		String str5 = "ab"+"c"; 
+		Integer b=4;
+		Integer c=4;
+		Integer d=5;
+		Integer f=4+1;
+		System.out.println(d==f);  //true
+		System.out.println(b.equals(c)); //true
+		System.out.println(b==c); 	//true
+		System.out.println(str1 == str2); //false
+		System.out.println(str1 == str3); //false
+		System.out.println(str3 == str4); //true 
+		System.out.println(str4 == str5); //true 
+	}
+}
