9ef546ef72431ad661dbdfe3f509d77687eb33b0
gaogang <gaogang@asiainfo.com>
2016-10-20 15:23:28 +0800

    合并冲突，并且修改代码

--- src/Wer.java
+++ src/Wer.java
@@ -3,10 +3,10 @@ public class Wer {
 
 	public static void main(String[] args) {
 
-		
+		System.out.println("hello world");
 	/*	
 		String str="123;345;456";
-		
+		sadfsdfsdafdsdasdfdsafdasdfsaddfasdfsdfdsdfdsdfdsdfdsfsfasdfadf
 		String[] a=StringUtils.splitPreserveAllTokens(str,";");
 		System.out.println(a.toString());
 		System.out.println(a.length);
