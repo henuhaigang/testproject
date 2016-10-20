7645711c657e45ec6907054d596907e62f48f3e1
gaogang <gaogang@asiainfo.com>
2016-10-20 14:52:28 +0800

    2

--- src/Wer.java
+++ src/Wer.java
@@ -1,4 +1,17 @@
 
 public class Wer {
 
+	public static void main(String[] args) {
+
+		但是犯得上犯得上发射点法士大夫士大夫
+	/*	
+		String str="123;345;456";
+		
+		String[] a=StringUtils.splitPreserveAllTokens(str,";");
+		System.out.println(a.toString());
+		System.out.println(a.length);
+		for(int i=0;i<a.length;i++){
+			System.out.println(a[i]+"b");
+		}*/
+	}
 }
